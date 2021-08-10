1ce35335bc55549e9e71c8d5fd15f166689234f7
Twiitchter <andylutze6090@gmail.com>
2021-08-10 19:52:44 +1000

    Server.cfg cleanup and adding /setjob

--- server/_commands.lua
+++ server/_commands.lua
@@ -32,7 +32,7 @@ RegisterCommand('switch', function(source)
         TriggerClientEvent("Client:Character:Switch")
         TriggerEvent("Server:Character:Switch")
     end)
-end, false)
+end, true)
 
 -- ====================================================================================--
 
@@ -84,4 +84,28 @@ RegisterCommand('kick', function(source, ...)
 end, true)
 
 -- ====================================================================================--
+
+TriggerEvent("chat:addSuggestion", "/setjob", "Admin Permission(s) Required.", {{
+    name = "TargetID",
+    jobname = "JobName",
+    jobgrade = "JobGrade",
+    help = "Server ID [1], Job Name [2], Job Grade [3] : Use a Space between each variable. Expecting 3 Variables"
+}})
+
+RegisterCommand('setjob', function(source, ...)
+    local src = tonumber(source)
+    local args = {...}
+    if (type(args[1]) ~= 'number') then
+        TriggerClientEvent("Client:Notify", src, 'Invalid Server ID :'..args[1]..', Used for /setjob command.')
+    else
+        if c.job.Exist(args[2], args[3]) then
+            local xPlayer = c.data.GetPlayer(args[1])
+            xPlayer.SetJob(args[2], args[3])
+            TriggerClientEvent("Client:Notify", src, 'ID:'..args[1]..', JobName: '..args[2]..', JobGrade: '..args[3]..'.)
+        else
+            TriggerClientEvent("Client:Notify", src, 'JobName: '..args[2]..' or JobGrade: '..args[3]..', does not exist.)
+        end
+    end    
+end, true)
+
 
