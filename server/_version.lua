2550123bf21ebf7a6746dabdce37e0d798c4c0a9
Twiitchter <andylutze6090@gmail.com>
2021-08-21 09:51:08 +1000

    test 2

--- server/_version.lua
+++ server/_version.lua
@@ -1,18 +1,18 @@
 -- ====================================================================================--
 --  MIT License 2020 : Twiitchter
 -- ====================================================================================--
+--	
+local version = c.module.GetVersion()
+local OnlineVersion = nil
 
-PerformHttpRequest( "https://github.com/Ingenium-Games/ore/version.json", function(err, text, headers)
-	Citizen.Wait(1250)
-    --	
-    local version = c.module.GetVersion()
-    local data = json.decode(text)
+PerformHttpRequest("https://github.com/Ingenium-Games/ore/version.txt", function(err, text, headers)
+    Citizen.Wait(1250)
+    OnlineVersion = json.decode(text)
     --
-	c.debug(" ^0[ ^8Update Checking^0 ] ")
-	if (data.version ~= version) then
+    c.debug(" ^0[ ^8Update Checking^0 ] ")
+    if (OnlineVersion.version ~= version) then
         print("Wrong Version Installed.")
     else
-        print(version.. " == " ..data.version)
+        print(version.. " == " ..OnlineVersion.version)
     end
-end)
-
+end, "GET")
