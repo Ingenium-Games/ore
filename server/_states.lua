d510685c6815465bc351cb19304cf16511d5803b
Twiitchter <andylutze6090@gmail.com>
2021-09-04 22:55:12 +1000

    Whoops

--- server/_states.lua
+++ server/_states.lua
@@ -21,11 +21,10 @@ math.randomseed(c.Seed)
 function c.state.AddState(name, value, description, effects, actions)
     if not c.states[name] then
         table.insert(c.states, name)
-        value = {["description"] = description, ["effect"] = effects or nil, ["action"] = actions or nil}
-        c.states[name] = value
+        c.states[name] = {}
+        c.states[name][value] = {["description"] = description, ["effect"] = effects or nil, ["action"] = actions or nil}
     else
-        value = {["description"] = description, ["effect"] = effects or nil, ["action"] = actions or nil}
-        c.states[name] = value
+        c.states[name][value] = {["description"] = description, ["effect"] = effects or nil, ["action"] = actions or nil}
     end
 end
 
@@ -101,15 +100,15 @@ local T = {[1] = {"",function() print('this is a effect') end, function() print(
 local S = {[1] = {""}, [2] = {""}, [3] = {""},[4] = {""}, [5] = {""}, [6] = {""},[7] = {""}, [8] = {""}, [9] = {""}, [10] = {""}}
 
 for k,v in pairs(H) do
-    c.state.AddState("Hunger", v, v[1] or nil, v[2] or nil, v[3] or nil)
+    c.state.AddState("Hunger", k, v[1] or nil, v[2] or nil, v[3] or nil)
 end
 
 for k,v in pairs(T) do
-    c.state.AddState("Thirst", v, v[1] or nil, v[2] or nil, v[3] or nil)
+    c.state.AddState("Thirst", k, v[1] or nil, v[2] or nil, v[3] or nil)
 end
 
 for k,v in pairs(S) do
-    c.state.AddState("Stress", v, v[1] or nil, v[2] or nil, v[3] or nil)
+    c.state.AddState("Stress", k, v[1] or nil, v[2] or nil, v[3] or nil)
 end
 
 c.debug(c.table.Dump(c.states.Hunger))
